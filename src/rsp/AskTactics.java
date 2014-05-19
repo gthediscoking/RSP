0f0598aaebe7b0b54eaa7bc26a02129c56c3bea7
gthediscoking <discoking@ymail.com>
2014-05-19 21:59:04 +0900

    - AskTactics is modified. - CyclicTactics is added.

--- src/rsp/AskTactics.java
+++ src/rsp/AskTactics.java
@@ -22,10 +22,44 @@ public class AskTactics implements Tactics {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		System.out.println("\nジャンケンの手を入力して下さい。");
-		System.out.println("\nジャンケンの手を入力して下さい。");
+		System.out.println("0: グー");
+		System.out.println("1: チョキ");
+		System.out.println("2: パー\n");
+		System.out.println("? ");
+		
+		// ジャンケンの手
+		int hand = 0;
+		
+		while(true)
+		{
+			try
+			{
+				// 入力文字列を受け取る
+				String inputStr = br.readLine();
+				
+				// 入力文字列を数値に変換する
+				hand = Integer.parseInt(inputStr);
+				
+				// 0〜2のいずれかが入力されたらループを抜ける
+				if ( hand == Player.STONE
+				  || hand == Player.SCISSORS
+				  || hand == Player.PAPER)
+				{
+					break;
+				}
+				else
+				{
+					System.out.println("入力が正しくありません。再度入力して下さい。");
+					System.out.println("? ");
+				}
+			}
+			catch(Exception ex)
+			{
+				System.out.println("入力が正しくありません。再度入力して下さい。");
+				System.out.println("? ");
+			}
+		}
 		
 		return 0;
 	}
-	
-
 }
---
+++ src/rsp/CyclicTactics.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package rsp;
+
+/**
+ * @author Hideki
+ *
+ */
+public class CyclicTactics implements Tactics {
+	/**
+	 * @see rsp.Tactics#readTactics()
+	 */
+	@Override
+	public int readTactics() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
